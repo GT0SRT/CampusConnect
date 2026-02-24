044658022891e57490c5a79da77e5b4c7c7e6cf1
Gourav Makode <makodegourav1@gmail.com>
2026-02-24 16:36:38 +0530

    call ignorance

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,9 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
+       workbox: {
+         globIgnores: ['**/InterviewCallRoom-*.js'],
+      },
       manifest: {
         name: 'campus connect',
         short_name: 'campusnet',
